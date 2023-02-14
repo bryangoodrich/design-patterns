0212cb381cb66608bf22ebddb21a4b4e1dd01119
bryangoodrich <goodrich.bryan@gmail.com>
2023-02-13 17:29:47 -0800

    Add chapter 1 python script template

---
+++ Chapter 01 Strategy/chapter01.py
@@ -0,0 +1,16 @@
+"""
+Chapter 1 - Strategy
+
+This script is intended for both review and execution to demonstrate the examples show in the textbook.
+
+<TBD: Brief summary of example in book here>
+"""
+
+
+def main():
+    """TBD Fill in the overview and examples from the book in a Pythonic way"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
